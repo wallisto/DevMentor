b73779ef12816bebfaf59fa847de3408ba60d320
wallisto <wallisthomas@hotmail.com>
2014-11-17 19:31:33 +0000

    Halfway through Entity Framework Lab. Got to Step 10 in Part 1.

--- Labs/EF/Before/ORM/Program.cs
+++ Labs/EF/Before/ORM/Program.cs
@@ -9,6 +9,16 @@ namespace ORM
     {
         static void Main(string[] args)
         {
+            Pubs pubs = new Pubs();
+
+            using (pubs)
+            {
+                foreach (Publisher pub in pubs.Publishers)
+                {
+                    Console.WriteLine(pub);
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ Labs/EF/Before/ORM/PublisherEx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ORM
+{
+    public partial class Publisher
+    {
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}

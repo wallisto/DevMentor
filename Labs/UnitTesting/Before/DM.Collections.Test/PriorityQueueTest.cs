206e3a1c11167ea77d60cf1e86ac8f14438d16d9
wallisto <wallisthomas@hotmail.com>
2014-11-18 10:18:21 +0000

    Halfway through Unit Testing lab. Done Part 1.

---
+++ Labs/UnitTesting/Before/DM.Collections.Test/PriorityQueueTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DM.Collections;
+
+namespace DM.Collections.Test
+{
+    [TestClass]
+    public class PriorityQueueTest
+    {
+        private PriorityQueue<int> pQueue;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            pQueue = new PriorityQueue<int>();
+        }
+
+        [TestMethod]
+        public void Ctor_WhenCreated_ShouldHaveCount0()
+        {
+            Assert.AreEqual(0, pQueue.Count);
+        }
+
+        [TestMethod]
+        public void Enqueue_WhenCalled_IncrementsCountByOne()
+        {
+            pQueue.Enqueue(1);
+
+            Assert.AreEqual(1, pQueue.Count);
+        }
+
+        [TestMethod]
+        public void Dequeue_WhenCalled_DecrementsCountByOne()
+        {
+            pQueue.Enqueue(1);
+            pQueue.Enqueue(4);
+            pQueue.Dequeue();
+
+            Assert.AreEqual(1, pQueue.Count);
+        }
+
+        [TestMethod]
+        public void Dequeue_WhenCalledAfterEnqueue_ShouldReturnCountToInitialValue()
+        {
+            int count = pQueue.Count;
+            pQueue.Enqueue(5);
+
+            pQueue.Dequeue();
+
+            Assert.AreEqual(count, pQueue.Count);
+        }
+
+        [TestMethod]
+        public void Dequeue_WhenCalledAfterEnqueue_ReturnsEnqueuedItem()
+        {
+            pQueue.Enqueue(5);
+
+            var item = pQueue.Dequeue();
+
+            Assert.AreEqual(5, item);
+        }
+
+        [TestMethod]
+        public void Dequeue_WhenCalledAfterTwoEnqueues_ReturnsFirstEnqueuedItem()
+        {
+            pQueue.Enqueue(5);
+            pQueue.Enqueue(23);
+
+            var item = pQueue.Dequeue();
+
+            Assert.AreEqual(5, item);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Dequeue_WhenQueueIsEmpty_ThrowsException()
+        {
+            pQueue.Dequeue();
+        }
+    }
+}
---
+++ Labs/UnitTesting/Before/DM.Collections/PriorityQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DM.Collections
+{
+    public class PriorityQueue<T>
+    {
+        public int Count { get { return items.Count; } }
+        private List<T> items = new List<T>();
+        
+        public void Enqueue(T item)
+        {
+            this.items.Add(item);
+        }
+
+        public object Dequeue()
+        {
+            try
+            {
+                T item = items[0];
+                items.RemoveAt(0);
+                return item;
+            }
+            catch
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
